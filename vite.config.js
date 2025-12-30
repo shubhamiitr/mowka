ed81c63968a92662cc2496e10344f1259ff1363b
kansal <kansalshubham46@gmail.com>
2025-12-30 12:01:30 +0530

    feat: Integrate React Snap for pre-rendering, updating build scripts and hydration logic.

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { SITE_CONTENT } from './src/constants/content'
 
 export default defineConfig({
   plugins: [
     react()
   ],
+  build: {
+    target: 'es2015'
+  }
 })
 
